7bf4dde899d8c30d1b8b1e00dc922cac60da3d6a
Pablogabi07 <83091813+Pablogabi07@users.noreply.github.com>
2024-10-30 10:42:07 -0300

    Update script.js

--- styles/script.js
+++ styles/script.js
@@ -1,11 +1,4 @@
-const card = document.getElementById('birthday-card');
-const flipBtn = document.getElementById('flip-btn');
-const flipBackBtn = document.getElementById('flip-back-btn');
-
-flipBtn.addEventListener('click', () => {
-  card.classList.add('flipped');
-});
-
-flipBackBtn.addEventListener('click', () => {
-  card.classList.remove('flipped');
-});
+function flipCard() {
+  const card = document.querySelector('.card');
+  card.classList.toggle('flipped');
+}
